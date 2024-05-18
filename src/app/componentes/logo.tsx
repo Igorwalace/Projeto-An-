208520c0d0b5705ffd61aab1301f290552d88008
igoor <igorwallacy3@gmail.com>
2024-05-18 16:06:52 -0300

    link na logo

--- src/app/componentes/logo.tsx
+++ src/app/componentes/logo.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 const Logo = () => {
     return (
-        <Link href='/' className='hover:scale-105 duration-200 text-[var(--verde)]'>
+        <Link href='https://meu-website-igor.vercel.app/' target='_blank' className='hover:scale-105 duration-200 text-[var(--verde)]'>
             {/* logo */}
             <h1 className={`font-black md:text-3xl text-2xl ${russo_One.className} `} >Anõ</h1>
         </Link>
