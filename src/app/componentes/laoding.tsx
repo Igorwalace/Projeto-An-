a304b45f2b49ae7edde959b09eaf0dd52745b7a6
igoor <igorwallacy3@gmail.com>
2024-05-18 22:33:18 -0300

    bottom e right de loading

--- src/app/componentes/laoding.tsx
+++ src/app/componentes/laoding.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
     return (
         <div className="fixed bottom-0 left-0 top-0 right-0 flex items-center justify-center z-50">
-            <span className="loader absolute bottom-10 right-14"></span>
+            <span className="loader absolute md:bottom-10 md:right-10 bottom-5 right-7"></span>
         </div>
     )
 }
