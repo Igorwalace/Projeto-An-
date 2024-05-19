8b5c0c3ae0c622cce658f522ffc61a13ea1d98ff
igoor <igorwallacy3@gmail.com>
2024-05-19 12:22:01 -0300

    largura definida no storys de carregamento

--- src/app/componentes/storys.tsx
+++ src/app/componentes/storys.tsx
@@ -30,10 +30,10 @@ const Storys = () => {
             {
                 firestoreStory.length < 1 &&
                 <>
-                    <div className="flex items-center justify-center flex-col">
-                        <main className='md:w-full w-[87%] bg-[#e1e1e1] h-[200px] rounded-xl' ></main>
-                        <main className='w-[87%] md:w-full bg-[#e1e1e1] h-[200px] rounded-xl my-2' ></main>
-                        <main className='w-[87%] md:w-full bg-[#e1e1e1] h-[200px] rounded-xl' ></main>
+                    <div className="flex md:items-start items-center justify-center flex-col">
+                        <main className='md:w-[95%] w-[87%] bg-[#e1e1e1] h-[200px] rounded-xl' ></main>
+                        <main className='w-[87%] md:w-[95%] bg-[#e1e1e1] h-[200px] rounded-xl my-2' ></main>
+                        <main className='w-[87%] md:w-[95%] bg-[#e1e1e1] h-[200px] rounded-xl' ></main>
                     </div>
                     <Loading />
                 </>
